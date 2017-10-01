ee1461d66559a09a164fe5371b717cb8f3c7d6d6
tatwd <leoking9641@hotmail.com>
2017-10-01 10:45:26 +0800

    Add `Practice5`

---
+++ ASP.NET/Practices/Practice/Practice5/Cookie.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Cookie.aspx.cs" Inherits="Cookie" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            姓名：<asp:TextBox ID="txtName" runat="server"></asp:TextBox>
+            <br />
+            邮箱：<asp:TextBox ID="txtEmail" runat="server"></asp:TextBox>
+            <br />
+            电话：<asp:TextBox ID="txtTelephone" runat="server"></asp:TextBox>
+            <br />
+            <asp:Button ID="btnSaveCookie" runat="server" OnClick="btnSaveCookie_Click" Text="保存Cookie" />
+            <asp:Button ID="btnViewCookie" runat="server" OnClick="btnViewCookie_Click" Text="读取Cookie" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ ASP.NET/Practices/Practice/Practice5/Cookie.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Cookie : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btnSaveCookie_Click(object sender, EventArgs e)
+    {
+        // 利用HttpCookie来保存Cookie
+        HttpCookie cookie = new HttpCookie("userInfo");
+
+        cookie.Values["UserName"]      = txtName.Text.Trim();
+        cookie.Values["UserEmail"]     = txtEmail.Text.Trim();
+        cookie.Values["UserTelephone"] = txtTelephone.Text.Trim();
+
+        txtName.Text = "";
+        txtEmail.Text = "";
+        txtTelephone.Text = "";
+
+        // 指定Cookie保存时间，否则保存在浏览器进程的内存中
+        //cookie.Expires = System.DateTime.Now.AddHours(1);
+
+        Response.Cookies.Add(cookie);
+    }
+
+    protected void btnViewCookie_Click(object sender, EventArgs e)
+    {
+        HttpCookie cookie = Request.Cookies["userInfo"];
+
+        if (cookie != null)
+        {
+            string userName      = cookie["UserName"];
+            string userEmail     = cookie["UserEmail"];
+            string userTelephone = cookie["UserTelephone"];
+
+            if (userName != null && userEmail != null && userTelephone != null)
+            {
+                txtName.Text      = Server.HtmlEncode(userName);
+                txtEmail.Text     = Server.HtmlEncode(userEmail);
+                txtTelephone.Text = Server.HtmlEncode(userTelephone);
+            }
+        }
+    }
+}
