686d66e84e43c2654be9e8c39fc547792370de8f
tatwd <leoking9641@hotmail.com>
2017-10-23 17:21:38 +0800

    Add and create some constructors of `Connector`.

---
+++ ASP.NET/Connector/App_Code/Connector.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+// using System.Collections.Generic;
+// using System.Linq;
+// using System.Web;
+
+using System.Data;
+using System.Data.SqlClient; // for mssql
+
+
+// Summary
+//   Connector 连接器 - 封装一些数据库连接的通用方法.
+//
+public class Connector
+{
+    // Summary:
+    //   Type of database.
+    private string DbType { set; get; }
+
+    // Summary:
+    //   The database connection.
+    private string DbConnection { set; get; }
+
+    // Summary:
+    //   The type of database is 'mssql'.
+    public Connector()
+    {
+        this.DbType = "mssql";
+    }
+
+    // Summary:
+    //   This constructor will set the type of database by a parameter.
+    //
+    // Parameters:
+    //   dbType: the type of database - 'mssql' or 'mysql'.
+    public Connector(string dbType)
+    {
+        this.DbType = dbType;
+    }
+}
