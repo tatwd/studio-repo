6e45e5fe9750a0dae389da3fc588ddf81153351e
tatwd <leoking9641@hotmail.com>
2018-03-04 20:51:15 +0800

    update

--- DesignPattern/Task/AbstractFactoryPattern/Program.cs
+++ DesignPattern/Task/AbstractFactoryPattern/Program.cs
@@ -10,9 +10,9 @@ namespace AbstractFactoryPattern
     {
         static void Main(string[] args)
         {
-            FoodFactory.GetFoods("江西", "宫爆鸡丁", "鱼香茄子", "西红柿炒蛋", "麻婆豆腐");
-            FoodFactory.GetFoods("四川", "宫爆鸡丁", "鱼香茄子", "西红柿炒蛋", "麻婆豆腐");
-            FoodFactory.GetFoods("广东", "宫爆鸡丁", "鱼香茄子", "西红柿炒蛋", "麻婆豆腐");
+            FoodFactory.GetFoods("江西", "麻婆豆腐");
+            FoodFactory.GetFoods("四川", "宫爆鸡丁", "鱼香茄子");
+            FoodFactory.GetFoods("广东", "宫爆鸡丁", "鱼香茄子", "西红柿炒蛋");
             FoodFactory.GetFoods("上海", "宫爆鸡丁", "鱼香茄子", "西红柿炒蛋", "麻婆豆腐");
         }
     }
