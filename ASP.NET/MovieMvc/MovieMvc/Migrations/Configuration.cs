976862c6db55bd631d94df35acd0e5e2d3aa3986
tatwd <leoking9641@hotmail.com>
2018-03-06 21:26:53 +0800

    add new `Rating` to db and update index view

---
+++ ASP.NET/MovieMvc/MovieMvc/Migrations/Configuration.cs
@@ -0,0 +1,40 @@
+namespace MovieMvc.Migrations
+{
+    using Models;
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<MovieMvc.Models.MovieDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(MovieMvc.Models.MovieDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+
+            context.Movies.AddOrUpdate(
+                m => m.Id,
+                new Movie
+                {
+                    Name = "ÐÂÁúÃÅ¿ÍÕ»",
+                    Rating = "8.5"
+                });
+        }
+    }
+}
--- ASP.NET/MovieMvc/MovieMvc/Models/Movie.cs
+++ ASP.NET/MovieMvc/MovieMvc/Models/Movie.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Data.Entity;
 
 namespace MovieMvc.Models
 {
@@ -9,5 +6,11 @@ namespace MovieMvc.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Rating { get; set; }
     }
+
+    public class MovieDbContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+    } 
 }
