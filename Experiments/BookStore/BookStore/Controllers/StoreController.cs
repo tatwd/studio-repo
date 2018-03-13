54d559cba588f5ea050801b2d544c344fc86b822
tatwd <leoking9641@hotmail.com>
2018-03-13 20:24:58 +0800

    add

---
+++ Experiments/BookStore/BookStore/Controllers/StoreController.cs
@@ -0,0 +1,29 @@
+﻿using BookStore.Models;
+using System.Web.Mvc;
+
+namespace BookStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        BookStoreEntities context = new BookStoreEntities();
+
+        // GET: Store
+        public ActionResult Index()
+        {
+            var books = context.Books;
+
+            // return Json(books, JsonRequestBehavior.AllowGet);
+            return View(books);
+        }
+
+        public ActionResult Order()
+        {
+            return View();
+        }
+
+        public ActionResult Details()
+        {
+            return View();
+        }
+    }
+}
---
+++ Experiments/BookStore/BookStore/README.md
@@ -0,0 +1,4 @@
+﻿## Change Log
+
+1. New a `StoreController`.
+2. Add data models with Entity Framework and using databsae first.
