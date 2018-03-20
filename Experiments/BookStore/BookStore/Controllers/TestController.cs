2a80af757589a44c38f74d822ba0fd8ed3eb053d
tatwd <leoking9641@hotmail.com>
2018-03-20 11:46:56 +0800

    add

---
+++ Experiments/BookStore/BookStore/Controllers/TestController.cs
@@ -0,0 +1,35 @@
+﻿using BookStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookStore.Controllers
+{
+    public class TestController : Controller
+    {
+        BookStoreEntities context = new BookStoreEntities();
+        
+        // GET: Test
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        // GET: /test/books
+        public ActionResult Books()
+        {
+            // context.Configuration.ProxyCreationEnabled = false;
+            // var books = context.Books.ToList();
+
+            var books = from b in context.Books
+                        select new { b.BookId, b.Title, b.BookCoverUrl, b.CategoryId, b.Price, b.Authors };
+
+            //var books = context.Books.SelectMany()
+
+            return Json(books.ToList(), JsonRequestBehavior.AllowGet);
+        }
+    }
+}
